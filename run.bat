362775dbf650dd143b7387ff9c3f509a52f61863
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2024-06-25 21:28:06 +0800

    first commit

---
+++ run.bat
@@ -0,0 +1,3 @@
+gcc -std=c99 -Wall -Werror -o main.exe src\stack.c src\priority.c src\tokenizer.c src\infixToPostfix.c src\queue.c src\evaluatePostfix.c src\main.c
+main
+pause
---
+++ run.sh
@@ -0,0 +1,2 @@
+gcc -Wall -Werror -o main src/*.c
+./main
