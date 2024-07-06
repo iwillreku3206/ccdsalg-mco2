544107bf4c128f9ea223a3b4111ba08b4307be6c
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2024-07-07 00:49:49 +0800

    add file fn proto

---
+++ src/file.c
@@ -0,0 +1,6 @@
+#include "file.h"
+#include <stdio.h>
+
+Graph *read_graph(String fileName) {
+  
+}
---
+++ src/file.h
@@ -0,0 +1,8 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "graph.h"
+
+Graph *read_graph(String fileName);
+
+#endif
