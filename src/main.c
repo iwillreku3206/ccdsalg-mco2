51e06a27ee51a5835d34b462cda6fa4c5aa7570e
JeyM <jeymson8000@gmail.com>
2024-07-11 20:34:18 +0800

    will modify searches to print names

--- src/main.c
+++ src/main.c
@@ -14,9 +14,17 @@ int main() {
     // I just contained everything within here for the time being
     int startIndex = graph_setup(&graph, nodes);
 	
+	FILE *traversals = fopen("../TRAVERSALS.txt", "w");
+	if (traversals == NULL) {
+        printf("Writing error.\n");
+        return 1;
+    }
+
 	bfs(&graph, &bfsTree, startIndex);
     printf("\n");
 	dfs(&graph, &bfsTree, startIndex);
 
+
+	fclose(traversals);
 	return 0;
 }
