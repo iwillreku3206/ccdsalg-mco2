47d9f9052781b4dbdb14ac1aad4aa38c57297a41
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2024-07-15 23:27:26 +0800

    simplify bfs code

--- src/bfs.c
+++ src/bfs.c
@@ -29,38 +29,27 @@ void bfs(Graph *graph, Graph *bfsTree, int from, FILE *file, Vertex nodes[]) {
 	while (!queue_empty(&queue)) {
 		int vertex = queue_dequeue(&queue).value;
 		fprintf(file, "%s ", nodes[vertex].name);
-		printf("%s ", nodes[vertex].name);
 
-		// get the number of next vertices
-		int numNextVertices = 0;
-		for (int i = 0; i < graph->vertexCount; i++) {
-			if (graph->adjacencyMatrix[vertex][i] == true &&
-				discovered[i] == false) {
-				numNextVertices++;
-			}
-		}
-
-		// next, place them all in an array
-		int nextVertices[numNextVertices];
-		int tmp = 0;
+		// place all discoverable vertices into an array
+		int nextVertices[graph->vertexCount];
+		int nextCount = 0;
 
 		for (int i = 0; i < graph->vertexCount; i++) {
 			if (graph->adjacencyMatrix[vertex][i] == true &&
 				discovered[i] == false) {
-				nextVertices[tmp] = i;
-				tmp++;
+				nextVertices[nextCount] = i;
+				nextCount++;
 			}
 		}
 
 		// next, sort the array
-		sort_by_name(graph->vertexList, nextVertices, numNextVertices);
+		sort_by_name(graph->vertexList, nextVertices, nextCount);
 
-    if (numNextVertices > 0) {
-		// enqueue the lowest vertex in the list
-		queue_enqueue(&queue, nextVertices[0]);
-		graph_add_edge(bfsTree, vertex, nextVertices[0]);
-		discovered[nextVertices[0]] = true;
-
-    }
+		if (nextCount > 0) {
+			// enqueue the lowest vertex in the list
+			queue_enqueue(&queue, nextVertices[0]);
+			graph_add_edge(bfsTree, vertex, nextVertices[0]);
+			discovered[nextVertices[0]] = true;
+		}
 	}
 }
