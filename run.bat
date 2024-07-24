d95b5d6824c6b695604e2d1bc52623cefbe0dfd2
JeyM <jeymson8000@gmail.com>
2024-07-24 22:42:18 +0800

    raylib done

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-gcc -std=c99 -Wall -Werror -lraylib -o main.exe src\display.c src\util.c src\stack.c src\queue.c src\fdgraphviz.c src\main.c src\file.c src\dfs.c src\graph.c src\bfs.c 
+gcc -std=c99 -Wall -Werror -L. -lraylib -o main.exe src\display.c src\util.c src\stack.c src\queue.c src\fdgraphviz.c src\main.c src\file.c src\dfs.c src\graph.c src\bfs.c 
 main
 pause
