be579e486dbaaac3505072adc7ad236f544079ad
Latzero <tabehiroyuki@gmail.com>
2024-07-24 19:11:09 +0800

    fixed force again

---
+++ src/display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include "C:\raylib\raylib\src\raylib.h"
+#include "fdgraphviz.h"
+#include <stdio.h>
+
+void draw_edge(Node a, Node b);
+void draw_node(Node a);
+void open_window(Graph g);
+
+#endif
