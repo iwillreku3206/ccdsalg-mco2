1fe083a74b6d4c9084875655766b791c5c3c9ad1
Latzero <tabehiroyuki@gmail.com>
2024-07-26 00:26:41 +0800

    Added comments

--- src/display.c
+++ src/display.c
@@ -1,5 +1,30 @@
+/***************************************************************************
+This is to certify that this project is my own work, based on my personal efforts in studying and
+applying the concepts learned. I have constructed the functions and their respective algorithms
+and corresponding code by myself. The program was run, tested, and debugged by my own
+efforts. I further certify that I have not copied in part or whole or otherwise plagiarized the
+work of other students and/or persons.
+ Hiroyuki Tabe, DLSU ID# 12207838
+***************************************************************************/
+
+/*
+ Description: This file is responsible for interaction with the raylib library
+ Programmed by: Hiroyuki Tabe
+ [Acknowledgements: 
+ 	https://stackoverflow.com/questions/69863663/how-do-i-link-and-compile-raylib-on-windows
+ 	https://www.youtube.com/watch?v=-F6THkPkF2I - Getting Started with Raylib - C Tutorial
+ 	https://www.reddit.com/r/raylib/comments/rh3zm4/center_text/
+    https://www.raylib.com/cheatsheet/cheatsheet.html
+ ]
+*/
+
 #include "display.h"
 
+/** draw_edge - function that draws an edge between two nodes
+ *  @param a - Node representing the start of the line drawing
+ *  @param b - Node representing the end of the line drawing
+ *  @param size - Thickness of the line
+ */
 void draw_edge(Node a, Node b, int size) {
     Vector2 c, d;
     c.x = a.x;
@@ -9,6 +34,11 @@ void draw_edge(Node a, Node b, int size) {
     DrawLineEx(c, d, size, BLACK);
 }
 
+/** draw_node - function that draws a Node into the window using its position and name
+ *  @param a - Node to be drawn
+ *  @param radius - Radius of the circle
+ *  @param fontSize - Font size of the text
+ */
 void draw_node(Node a, int radius, int fontSize) {
     Vector2 n;
     int measure;
@@ -21,6 +51,9 @@ void draw_node(Node a, int radius, int fontSize) {
     DrawText(a.name, n.x - measure / 2, n.y - fontSize / 2, fontSize, BLACK);
 }
 
+/** open_window - Opens a window using raylib
+ *  @param g - Graph that is the source of the vertices
+ */
 void open_window(Graph g) {
     int i, j, radius, edgeSize, fontSize;
     Node vertices[MAX_VERTICES];
--- src/fdgraphviz.c
+++ src/fdgraphviz.c
@@ -1,10 +1,42 @@
+/***************************************************************************
+This is to certify that this project is my own work, based on my personal efforts in studying and
+applying the concepts learned. I have constructed the functions and their respective algorithms
+and corresponding code by myself. The program was run, tested, and debugged by my own
+efforts. I further certify that I have not copied in part or whole or otherwise plagiarized the
+work of other students and/or persons.
+ Hiroyuki Tabe, DLSU ID# 12207838
+***************************************************************************/
+
+/*
+ Description: This file is responsible for the calculation regarding graph visualization.
+              This specific implementation of graph visualization uses something called a force-directed graph algorithm
+ Programmed by: Hiroyuki Tabe
+ [Acknowledgements: 
+ 	https://www.youtube.com/watch?v=WWm-g2nLHds&list=PLubYOWSl9mIvtnRjCCHP3wqNETTHYjQex&index=1 - Force-Directed Drawings (1/3): Algorithmic Framework | Visualization of Graphs - Lecture 3
+ 	https://www.youtube.com/watch?v=gJiSvGbH0CA&list=PLubYOWSl9mIvtnRjCCHP3wqNETTHYjQex&index=2 - Force-Directed Drawings (2/3): Eades Spring Embedder | Visualization of Graphs - Lecture 3
+    https://www.youtube.com/watch?v=JAe7Oscsp98&list=PLubYOWSl9mIvtnRjCCHP3wqNETTHYjQex&index=3 - Force-Directed Drawings (3/3): Fruchterman & Reingold | Visualization of Graphs - Lecture 3
+    https://www1.pub.informatik.uni-wuerzburg.de/demos/forceDirected.html
+    https://onlinelibrary.wiley.com/doi/10.1002/spe.4380211102 - Graph drawing by force-directed placement
+    https://stackoverflow.com/questions/62286695/is-there-a-simple-ish-algorithm-for-drawing-force-directed-graphs
+ ]
+*/
+
 #include "fdgraphviz.h"
 #include <stdio.h>
 
+/** get_magnitude - Gets the distance of a Node, assumes the Node starts from (0,0)
+ *  @param n - Node to get the distance
+ *  @return Distance as a real number
+ */
 float get_magnitude(Node n) {
     return sqrt(pow(n.x,2)+pow(n.y,2));
 }
 
+/** origin_vector - translates a vector from a --> b into a vector that starts from (0,0)
+ *  @param a - Starting vector
+ *  @param b - Ending vector
+ *  @return Node that starts from (0,0)
+ */
 Node origin_vector(Node a, Node b) {
     //vector from Node a --> b
     Node o;
@@ -16,6 +48,10 @@ Node origin_vector(Node a, Node b) {
     return o;
 }
 
+/** unit_vector - Changes a vector into its unit vector form (where the magnitude/distance = 1)
+ *  @param o - Origin vector
+ *  @return Origin vector with a magnitude of 1
+ */
 Node unit_vector(Node o) {
     Node u;
     float length = get_magnitude(o);
@@ -27,6 +63,10 @@ Node unit_vector(Node o) {
     return u;
 }
 
+/** initialize - initializes a graph and their vertices with a position on the grid
+ *  @param g - Graph that is the source of the vertices
+ *  @param vertices - Array of vertices containing their position and name
+ */
 void initialize(Graph g, Node vertices[]) {
     int i, x, y;
     srand(time(NULL));
@@ -44,6 +84,12 @@ void initialize(Graph g, Node vertices[]) {
     // printf("\n\n\n");
 }
 
+/** repulsion_force - Calculates the repulsion force of two vectors
+ *  @param a - Vector that the repulsion force will be applied to
+ *  @param b - Vector that is providing the repulsion force
+ *  @param strength - Strength of the repulsion force (generally works when it is between 1 to 2)
+ *  @return Node that represents the repulsion force
+ */
 Node repulsion_force(Node a, Node b, float strength) {
     Node f = unit_vector(origin_vector(b,a));
     float scalar;
@@ -57,6 +103,12 @@ Node repulsion_force(Node a, Node b, float strength) {
     return f;
 }
 
+/** attraction_force - Calculates the attraction force of two vectors
+ *  @param a - Vector that the attraction force will be applied to
+ *  @param b - Vector that is providing the attraction force
+ *  @param strength - Strength of the attraction force (generally works when it is between 1 to 1.5)
+ *  @return Node that represents the attraction force
+ */
 Node attraction_force(Node a, Node b, float strength) {
     Node f = unit_vector(origin_vector(a,b));
     float scalar;
@@ -69,6 +121,14 @@ Node attraction_force(Node a, Node b, float strength) {
     return f;
 }
 
+/** max_force - Finds the max amount of force throughout the entire graph
+ *  @param g - Graph that is the source of the vertices
+ *  @param vertices - Array of vertices containing their position and name
+ *  @param coolingFactor - Factor that scales down how much force is applied (it is exponentially decreasing with each iteration)
+ *  @param rStr - Strength of the repulsion force
+ *  @param aStr - Strength of the attraction force
+ *  @return Magnitude of the max force that a graph contains
+ */
 float max_force(Graph g, Node vertices[], float coolingFactor, float rStr, float aStr) {
     Node max, current, rCurrent, aCurrent, repulsion, attraction;
     int i, j;
@@ -106,10 +166,19 @@ float max_force(Graph g, Node vertices[], float coolingFactor, float rStr, float
     return get_magnitude(max);
 }
 
+/** cooling_factor - Exponentially decreases the max amount of force the graph is being subjected to.
+ *  @param factor - Initial cooling factor (generally 0.9 to 1)
+ *  @return New cooling factor
+ */
 float cooling_factor(float factor) {
     return factor * COOLING_FACTOR;
 }
 
+/** initialize_borders - initializes an array of Nodes to act as global forces preventing Nodes from the graph escaping the screen
+ *  @param borders - Array of vertices representing edge borders
+ *  @param nx - Number of border vertices responsible for the x-axis
+ *  @param ny - Number of border vertices responsible for the y-axis
+ */
 void initialize_borders(Node borders[], int nx, int ny) {
     int i;
     Node xBorderForces[nx], yBorderForces[ny];
@@ -149,6 +218,13 @@ void initialize_borders(Node borders[], int nx, int ny) {
 
 }
 
+/** global_force - Calculates the force that the border and center are afflicting upon the graph vertices
+ *  @param a - Node that the global force will apply its force on
+ *  @param borders - Array of vertices representing the border of the window
+ *  @param rStr - Strength of the repulsion force
+ *  @param aStr - Strength of the attraction force
+ *  @return Node that represents the global force
+ */
 Node global_force(Node a, Node borders[], float rStr, float aStr) {
     int i;
     Node repulsion, attraction, rForce, mid, force;
@@ -170,7 +246,14 @@ Node global_force(Node a, Node borders[], float rStr, float aStr) {
     return force;
 }
 
+/** force_directed - Main function that calculates the overall force that the graph will be subjected to
+ *  @param g - Graph that is the source of the vertices
+ *  @param vertices - Array of vertices containing their position and name
+ *  @param threshold - Force cutoff point where it ends the loop early if the max force being exerted is very small
+ *  @param maxIterations - Hard cutoff point where no more force calculation will be made
+ */
 void force_directed(Graph g, Node vertices[], float threshold, int maxIterations) {
+    // Initialization
     int i, j, k, x, y;
     Node repulsion, attraction, rForce, aForce;
     Node forces[MAX_VERTICES];
@@ -181,6 +264,7 @@ void force_directed(Graph g, Node vertices[], float threshold, int maxIterations
 
     initialize_borders(borderForces, 42, 18);
 
+    // Changing the force values slightly to account for more vertices in the graph
     if(g.vertexCount <= 10) {
         rStr = 1.8;
         aStr = 1.2;
@@ -199,7 +283,7 @@ void force_directed(Graph g, Node vertices[], float threshold, int maxIterations
             aForce.y = 0;
             for(k = 0; k < g.vertexCount; k++) {
                 if(j != k) {
-                    if(g.adjacencyMatrix[j][k] == true) {
+                    if(g.adjacencyMatrix[j][k] == true) { // Attraction force only applies when two vertices are adjacent
                         attraction = attraction_force(vertices[j], vertices[k], aStr);
                         aForce.x = aForce.x + attraction.x;
                         aForce.y = aForce.y + attraction.y;
@@ -209,6 +293,7 @@ void force_directed(Graph g, Node vertices[], float threshold, int maxIterations
                     rForce.y = rForce.y + repulsion.y;
                 }
             }
+            // Total force is calculated as the sum of all the repulsion forces + the sum of all the attraction forces on a given vector
             forces[j].x = rForce.x + aForce.x;
             forces[j].y = rForce.y + aForce.y;
             // printf("Force on %s: (%.2f, %.2f)\n", vertices[j].name, forces[j].x, forces[j].y);
@@ -221,6 +306,7 @@ void force_directed(Graph g, Node vertices[], float threshold, int maxIterations
             x = vertices[j].x + factor * forces[j].x;
             y = vertices[j].y + factor * forces[j].y;
 
+            // Preventing vertices from going outside the bounds of the window
             if(x + 60 > X_MAX) {
                 vertices[j].x = X_MAX - 60;
                 // x = vertices[j].x + factor * (forces[j].x + global_force(vertices[j], borderForces, rStr, aStr).x);
